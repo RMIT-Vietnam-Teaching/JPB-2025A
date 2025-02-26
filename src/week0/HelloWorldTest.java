77db44c85687d4bc63ae28377be86db144fe0af7
Minh Nguyen <nqminh@live.com>
2025-02-26 22:54:18 +0700

    Add the starter code

---
+++ src/week0/HelloWorldTest.java
@@ -0,0 +1,16 @@
+/* 
+ *  Example of a simple Java program that prints a message to the console
+ */
+
+// Class name must be same with the file name, HelloWorldTest is the class name in this case
+// All Java source code must be inside a class
+// Pay attention to indentation, semicolon (";"),
+// parenthesis ("(", ")") and braces ("{", "}")
+public class HelloWorldTest {
+   // Main method is the starting point of any Java program
+   public static void main(String[] args) {
+      // Print a greeting message
+      System.out.println("Hello World - Testing 1... 2... 3");
+      System.out.println("If you see the output, then Java is working!");
+   }
+}
